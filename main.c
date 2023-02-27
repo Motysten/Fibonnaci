5ed86c4cc2288f2c29417349f3619c715a3a1700
Motysten <mateo.demuynck@gmail.com>
2023-02-27 12:04:17 +0100

    First commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    unsigned long F0 = 0;
+    unsigned long F1 = 1;
+    unsigned long F2 = F0 + F1;
+
+    printf("%d \n", F0);
+    printf("%d \n", F1);
+
+    for (int i = 0; i < 45; i++) {
+        printf("%d \n", F2);
+        F0 = F1;
+        F1 = F2;
+        F2 = F0 + F1;
+    }
+
+    return 0;
+    
+}
