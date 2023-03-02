c5a3b68f71bdb0721eb54be722b0d5742e677729
Motysten <mateo.demuynck@gmail.com>
2023-03-02 11:27:30 +0100

    Create exo2.c

---
+++ exo2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char title[50];
+    float price;
+    int quantity;
+} Product;
+
+int main() {
+    Product products[150];
+    int num_products = 0;
+
+    char response[10];
+    do {
+        printf("Titre de l'article : ");
+        scanf("%s", products[num_products].title);
+        printf("Prix / unité : ");
+        scanf("%f", &products[num_products].price);
+        printf("Quantité : ");
+        scanf("%d", &products[num_products].quantity);
+
+        printf("Voulez-vous continuer ? (oui/non) : ");
+        scanf("%s", response);
+
+        num_products++;
+    } while (strcmp(response, "oui") == 0 && num_products < 100);
+
+    float total = 0;
+    printf("| Titre de l'article | Prix / unité | Quantité | Prix avec taxes |\n");
+    printf("-------------------------------------------\n");
+    for (int i = 0; i < num_products; i++) {
+        printf("| %s | %.2f € | %d | %.2f € |\n",
+               products[i].title, products[i].price, products[i].quantity,
+               products[i].price * products[i].quantity);
+        total += products[i].price * products[i].quantity;
+    }
+    printf("-------------------------------------------\n");
+    printf("| TOTAL | | | %.2f € |\n", total);
+
+    return 0;
+}
