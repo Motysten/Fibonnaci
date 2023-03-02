8b5572d63bd684063d3c4c50f3eae18d68cb858b
Motysten <mateo.demuynck@gmail.com>
2023-03-02 11:49:45 +0100

    end exo

--- exo2.c
+++ exo2.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <exercice.c>
 
 typedef struct {
     char title[50];
@@ -17,28 +16,28 @@ int main() {
     do {
         printf("Titre de l'article : ");
         scanf("%s", products[num_products].title);
-        printf("Prix / unité : ");
+        printf("Prix / unite : ");
         scanf("%f", &products[num_products].price);
-        printf("Quantité : ");
+        printf("Quantite : ");
         scanf("%d", &products[num_products].quantity);
 
         printf("Voulez-vous continuer ? (o/N) : ");
         scanf("%s", response);
 
         num_products++;
-    } while (mystrcmp(response, "o") == 0 && num_products < 100);
+    } while (strcmp(response, "o") == 0 && num_products < 100);
 
     float total = 0;
-    printf("| Titre de l'article | Prix / unité | Quantité | Prix avec taxes |\n");
+    printf("| Titre de l'article | Prix / unite | Quantite | Prix avec taxes |\n");
     printf("-------------------------------------------\n");
     for (int i = 0; i < num_products; i++) {
-        printf("| %s | %.2f € | %d | %.2f € |\n",
+        printf("| %s | %.2f euros | %d | %.2f euros |\n",
                products[i].title, products[i].price, products[i].quantity,
                products[i].price * products[i].quantity);
         total += products[i].price * products[i].quantity;
     }
     printf("-------------------------------------------\n");
-    printf("| TOTAL | | | %.2f € |\n", total);
+    printf("| TOTAL | | | %.2f euros |\n", total);
 
     return 0;
 }
